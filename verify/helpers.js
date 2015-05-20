57ec7b476b3a9f556a4f3fbf8383d8af19407348
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-20 05:23:33 -0700

    :art: Write Helpers as a class

--- verify/helpers.js
+++ verify/helpers.js
@@ -1,29 +1,27 @@
-var fs = require('fs')
-
 var ul = document.getElementById('verify-list')
 
-var addtoList = function (message, status) {
-  var li = document.createElement('li')
-  var newContent = document.createTextNode(message)
-  li.appendChild(newContent)
-  if (status) {
-    li.classList.add('verify-pass')
-  } else li.classList.add('verify-fail')
-  ul.appendChild(li)
-  // potentially do this with domify and push
-  // into an array and add to dom once
+class Helpers{
+  static addtoList(message, status) {
+    var li = document.createElement('li')
+    var newContent = document.createTextNode(message)
+    li.appendChild(newContent)
+    if (status) {
+      li.classList.add('verify-pass')
+    } else li.classList.add('verify-fail')
+    ul.appendChild(li)
+    // potentially do this with domify and push
+    // into an array and add to dom once
+  }
+  static markChallengeCompleted(){
+    var challengeBody = document.getElementById('challenge-body')
+    var challengeDesc = document.getElementById('challenge-desc')
+    var div = document.createElement('h2')
+    var newContent = document.createTextNode('COMPLETED!')
+    div.appendChild(newContent)
+    div.classList.add('completed-challenge')
+    // Add ID and make it so that it isn't added over and over to DOM
+    challengeBody.insertBefore(div, challengeDesc)
+  }
 }
 
-var markChallengeCompleted = function () {
-  var challengeBody = document.getElementById('challenge-body')
-  var challengeDesc = document.getElementById('challenge-desc')
-  var div = document.createElement('h2')
-  var newContent = document.createTextNode('COMPLETED!')
-  div.appendChild(newContent)
-  div.classList.add('completed-challenge')
-  // Add ID and make it so that it isn't added over and over to DOM
-  challengeBody.insertBefore(div, challengeDesc)
-}
-
-module.exports.markChallengeCompleted = markChallengeCompleted
-module.exports.addtoList = addtoList
+module.exports = Helpers
