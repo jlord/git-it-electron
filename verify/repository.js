d6acedc8f2bdc1d74d4ac9de61623a4d4d4d34c0
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-20 06:16:07 -0700

    :art: Rewrite repository.js

---
+++ verify/repository.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var helpers = require('./helpers.js')
+
+module.exports = function commitVerify(Path){
+  helpers.execute('git status', {cwd: Path}).then(function(Output){
+    if(Output.stderr.length) return Promise.reject(Output.stderr)
+    Progress.repository = true
+  }).catch(function(Error){
+    helpers.addToList(Error, false)
+  })
+}
