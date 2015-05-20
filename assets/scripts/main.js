35be5727ed5a6086d12661234b0390b13b4085cb
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-20 05:16:55 -0700

    :new: Add a new Progress class

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,10 +14,33 @@ class Main{
       Entry.addEventListener('click', function(e){
         e.preventDefault()
         Me.Shell.openExternal(Link)
-      });
+      })
     })
   }
 }
+class Progress {
+  constructor(){
+    let Me = this
+
+    this.Challenges = ['get_git', 'repository', 'commit_to_it']
+
+    this.Challenges.forEach(function(Challenge){ // Just in case we add more in the future
+      Object.defineProperty(Me, Challenge, {
+        get: function(){ return !!localStorage.getItem('git_it_' + Challenge) },
+        set: function(value){ localStorage.setItem('git_it_' + Challenge, value) }
+      })
+    })
+  }
+  get active(){
+    let Active = null
+    let Me = this
+    this.Challenges.forEach(function(Challenge){
+      if(Active === null && !Me[Challenge]) Active = Challenge
+    })
+    return Active
+  }
+}
+Progress = new Progress // I sometimes wish javascript had self-initializing
 document.addEventListener('DOMContentLoaded', function(){
   ( new Main() ).onLoaded()
-});
+})
