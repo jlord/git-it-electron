c0bf1ac640994f171369b79fd1eabd263c01365c
Jessica Lord <jessica.lord@github.com>
2015-05-31 19:15:45 -0700

    Clean up Commit cha verify, add completed steps

--- verify/commit_to_it.js
+++ verify/commit_to_it.js
@@ -13,16 +13,19 @@ var currentChallenge = 'commit_to_it'
 // check that they've commited changes
 
 module.exports = function commitVerify (path) {
+  if (!fs.lstatSync(path).isDirectory()) return addtoList('Path is not a directory', false)
   exec('git status', {cwd: path}, function (err, stdout, stdrr) {
     if (err) return addtoList(err.message, false)
     var show = stdout.trim()
 
-    if (show.match("Initial commit")) {
-      addtoList("Hmm, can't find\ncommitted changes.", false)
-    } else if (show.match("nothing to commit")) {
-      addtoList("Changes have been committed!", true)
+    if (show.match('Initial commit')) {
+      addtoList("Hmm, can't find committed changes.", false)
+    } else if (show.match('nothing to commit')) {
+      addtoList('Changes have been committed!', true)
+      markChallengeCompleted()
+      writeData(userData, currentChallenge)
     } else {
-      addtoList("Seems there are changes\nto commit still.", false)
+      addtoList('Seems there are changes to commit still.', false)
     }
   })
 }
