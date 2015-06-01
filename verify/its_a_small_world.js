9ea2415b75dedfc093edc3e8d5c524e94d75cabf
Jessica Lord <jessica.lord@github.com>
2015-05-31 19:02:48 -0700

    Add verify script

---
+++ verify/its_a_small_world.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+var request = require('request')
+var exec = require('child_process').exec
+
+var helper = require('../verify/helpers.js')
+var userData = require('../data.json')
+
+var url = 'http://reporobot.jlord.us/collab?username='
+
+var addtoList = helper.addtoList
+var markChallengeCompleted = helper.markChallengeCompleted
+var writeData = helper.writeData
+
+var currentChallenge = 'its_a_small_world'
+
+module.exports = function verifySmallWorldChallenge () {
+  exec('git config user.username', function (err, stdout, stdrr) {
+    if (err) return addtoList('Error: ' + err.message, false)
+    var username = stdout.trim()
+    collaborating(username)
+  })
+
+  // check that they've added RR as a collaborator
+
+  function collaborating (username) {
+    request(url + username, {json: true}, function (err, response, body) {
+      if (err) return addtoList('Error: ' + err.message, false)
+      if (!err && response.statusCode === 200) {
+        if (body.collab === true) {
+          addtoList('Reporobot has been added!', true)
+          markChallengeCompleted()
+          writeData(userData, currentChallenge)
+        } else addtoList("Reporobot doesn't have access to the fork", false)
+      }
+    })
+  }
+}
