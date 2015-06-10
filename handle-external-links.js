1a896ff426e14a6d84cbce7e6c8d6e9be243350a
Jessica Lord <jessica.lord@github.com>
2015-06-10 08:26:54 -0700

    Improve/fix opening external links

--- handle-external-links.js
+++ handle-external-links.js
@@ -3,16 +3,12 @@ var shell = require('shell')
 document.addEventListener('DOMContentLoaded', function (event) {
   var links = document.querySelectorAll('a[href]')
 
-  for (var l in links) {
-    var url = ''
-    if (typeof links[l] === 'object') {
-      url = links[l].getAttribute('href')
-    }
+  [].forEach.call(links, function (link) {
+    var url = link.getAttribute('href')
     if (url.indexOf('http:') > -1) {
-      var gohere = url // not sure why this had to be here
-      links[l].addEventListener('click', function (e) {
+      link.addEventListener('click', function (e) {
         e.preventDefault()
-        shell.openExternal(gohere)
+        shell.openExternal(url)
       })
     }
   }
