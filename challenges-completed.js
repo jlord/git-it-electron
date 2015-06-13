57a32ec29b5a8562dc97eb04a71cad850d28039f
Jessica Lord <jessica.lord@github.com>
2015-06-12 20:46:51 -0700

    Add clear all challenges button (not working)

--- challenges-completed.js
+++ challenges-completed.js
@@ -1,6 +1,8 @@
 var ipc = require('ipc')
 var fs = require('fs')
 
+var userData
+
 document.addEventListener('DOMContentLoaded', function (event) {
   ipc.send('getUserDataPath')
 
@@ -8,17 +10,34 @@ document.addEventListener('DOMContentLoaded', function (event) {
     console.log(path)
     updateIndex('./data.json')
   })
+
+  var clearButton = document.getElementById('clear-all-challenges')
+
+  clearButton.addEventListener('click', function (event) {
+    console.log('clear all')
+
+    for (var chal in userData) {
+      if (userData[chal].completed) {
+        userData[chal].completed = false
+        // var completedElement = '#' + chal + ' .completed-challenge-list'
+        // need to remove the span inside of each <li>
+      }
+    // fs.writeFile('./data.json', JSON.stringify(userData, null, ' '), function (err) {
+    //
+    // })
+    }
+  })
 })
 
 function updateIndex (path) {
   fs.readFile(path, function readFile (err, contents) {
     if (err) return console.log(err)
-    var userData = JSON.parse(contents)
+    userData = JSON.parse(contents)
 
     for (var chal in userData) {
       if (userData[chal].completed) {
         var currentText = document.getElementById(chal).innerHTML
-        var completedText = "<span class='compelted-challenge-list'>[ Completed ]</span>"
+        var completedText = "<span class='completed-challenge-list'>[ Completed ]</span>"
         document.getElementById(chal).innerHTML = completedText + ' ' + currentText
       }
     }
