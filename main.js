3c0a592d60d98b9ec5ab23878bdda9db9412eccf
Jessica Lord <jessica.lord@github.com>
2015-06-21 17:47:27 -0700

    Check for and create empty user data

--- main.js
+++ main.js
@@ -4,9 +4,13 @@ var crashReporter = require('crash-reporter')
 var Menu = require('menu')
 var ipc = require('ipc')
 var dialog = require('dialog')
+var fs = require('fs')
+var path = require('path')
 
 var darwinTemplate = require('./darwin-menu.js')
 var otherTemplate = require('./other-menu.js')
+
+var emptyData = require('./empty-data.json')
 
 var mainWindow = null
 var menu = null
@@ -23,9 +27,18 @@ app.on('ready', function appReady () {
   mainWindow = new BrowserWindow({width: 800, height: 900, title: 'Git-it'})
   mainWindow.loadUrl('file://' + __dirname + '/index.html')
 
+  var userDataPath = path.join(app.getPath('userData'), 'user-data.json')
+
+  fs.exists(userDataPath, function (exists) {
+    if (!exists) {
+      fs.writeFile(userDataPath, JSON.stringify(emptyData, null, ' '), function (err) {
+        if (err) return console.log(err)
+      })
+    }
+  })
+
   ipc.on('getUserDataPath', function (event) {
-    var userData = app.getPath('userData')
-    event.sender.send('haveUserDataPath', userData)
+    event.sender.send('haveUserDataPath', userDataPath)
   })
 
   ipc.on('open-file-dialog', function (event) {
