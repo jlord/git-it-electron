5cf56d09b7459f5e324436aa7183bc0a1c841a14
Jessica Lord <jessica.lord@github.com>
2015-06-21 17:48:03 -0700

    Use new user data API

--- challenges-completed.js
+++ challenges-completed.js
@@ -1,42 +1,31 @@
-var ipc = require('ipc')
-var fs = require('fs')
-
-var userData
+var userData = require('./user-data.js')
 
 document.addEventListener('DOMContentLoaded', function (event) {
-  ipc.send('getUserDataPath')
-
-  ipc.on('haveUserDataPath', function (path) {
-    updateIndex('./data.json')
-  })
+  var data = userData.getData()
+  updateIndex(data)
 
   var clearAllButton = document.getElementById('clear-all-challenges')
 
-  clearAllButton.addEventListener('click', function (event) {
-    for (var chal in userData) {
-      if (userData[chal].completed) {
-        userData[chal].completed = false
+  clearAllButton.addEventListener('click', function () {
+    for (var chal in data) {
+      if (data[chal].completed) {
+        data[chal].completed = false
         var completedElement = '#' + chal + ' .completed-challenge-list'
         document.querySelector(completedElement).remove()
       }
     }
-    fs.writeFile('./data.json', JSON.stringify(userData, null, ' '), function (err) {
+    userData.updateData(data, function (err) {
       if (err) return console.log(err)
     })
   })
 
-  function updateIndex (path) {
-    fs.readFile(path, function readFile (err, contents) {
-      if (err) return console.log(err)
-      userData = JSON.parse(contents)
-
-      for (var chal in userData) {
-        if (userData[chal].completed) {
-          var currentText = document.getElementById(chal).innerHTML
-          var completedText = "<span class='completed-challenge-list'>[ Completed ]</span>"
-          document.getElementById(chal).innerHTML = completedText + ' ' + currentText
-        }
+  function updateIndex (data) {
+    for (var chal in data) {
+      if (data[chal].completed) {
+        var currentText = document.getElementById(chal).innerHTML
+        var completedText = "<span class='completed-challenge-list'>[ Completed ]</span>"
+        document.getElementById(chal).innerHTML = completedText + ' ' + currentText
       }
-    })
+    }
   }
 })
