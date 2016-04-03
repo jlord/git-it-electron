ed0c3069555d3829642a0dcbbe90b819bb178a8f
Jessica Lord <jlord@github.com>
2016-04-02 17:16:42 -0700

    minWidth minHeight updates

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ app.on('window-all-closed', function appQuit () {
 })
 
 app.on('ready', function appReady () {
-  mainWindow = new BrowserWindow({"min-width": 800, "min-height": 600, width: 980, height: 760, title: 'Git-it', icon: iconPath })
+  mainWindow = new BrowserWindow({"minWidth": 800, "minHeight": 600, width: 980, height: 760, title: 'Git-it', icon: iconPath })
   mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   var userDataPath = path.join(app.getPath('userData'), 'user-data.json')
