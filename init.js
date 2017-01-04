42c0b7765c4e440419e6d9faf373ff997775c3b6
Brendan Forster <brendan@github.com>
2017-01-05 09:04:38 +1030

    add electron-compile to handle resources dynamically

---
+++ init.js
@@ -0,0 +1,3 @@
+// ...and that your main app is called ./src/main.js. This is written as if
+// you were going to `require` the file from here.
+require('electron-compile').init(__dirname, './main');
