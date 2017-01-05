d6de8f7f5a5826a113454d9adcac0f1f53bdbb0b
Brendan Forster <brendan@github.com>
2017-01-06 10:26:24 +1030

    add a simple test helper for extracting the repository for testing purposes

---
+++ tests/repository-helper.js
@@ -0,0 +1,23 @@
+var temp = require('temp').track()
+var fs = require('fs-extra')
+var path = require('path')
+
+function extractFixture(fixtureName) {
+  var fullPath = path.join(__dirname, 'fixtures', fixtureName)
+  // TODO: fail at this point if the path does not exist
+  var folder = temp.mkdirSync(fixtureName)
+  fs.copySync(fullPath, folder)
+  // rename the .git directory that we version controlled, so we can test it
+  var source = path.join(fullPath, '_git')
+  var destination = path.join(fullPath, '.git')
+  fs.renameSync(source, destination)
+  return folder
+}
+
+function createEmptyFolder(folderName) {
+  var folder = temp.mkdirSync(folderName)
+  return folder
+}
+
+module.exports.extractFixture = extractFixture
+module.exports.createEmptyFolder = createEmptyFolder
