2918414898ae63b1c1cd520b404e4ddd8b6ef386
Brendan Forster <brendan@github.com>
2017-01-07 10:35:20 +1100

    cleanup tests to use callbacks

--- tests/repository-helper.js
+++ tests/repository-helper.js
@@ -1,20 +1,20 @@
-var temp = require('temp').track()
+var temp = require('temp')
 var fs = require('fs-extra')
 var path = require('path')
 
-function extractFixture(fixtureName) {
+function extractFixture (fixtureName) {
   var fullPath = path.join(__dirname, 'fixtures', fixtureName)
   // TODO: fail at this point if the path does not exist
   var folder = temp.mkdirSync(fixtureName)
   fs.copySync(fullPath, folder)
   // rename the .git directory that we version controlled, so we can test it
-  var source = path.join(fullPath, '_git')
-  var destination = path.join(fullPath, '.git')
+  var source = path.join(folder, '_git')
+  var destination = path.join(folder, '.git')
   fs.renameSync(source, destination)
   return folder
 }
 
-function createEmptyFolder(folderName) {
+function createEmptyFolder (folderName) {
   var folder = temp.mkdirSync(folderName)
   return folder
 }
--- tests/repository.js
+++ tests/repository.js
@@ -4,27 +4,25 @@ var helper = require('./repository-helper')
 var verify = require('../lib/verify/repository')
 
 test('verifies the hello world repository', function (t) {
-  t.plan(2)
-
   var folder = helper.extractFixture('hello-world')
-  var result = verify(folder)
+  verify(folder, function (result) {
+    var expected = 'This is a Git repository!'
+    var first = result[0]
 
-  var expected = 'This is a Git repository!'
-  var actual = result[0].message
-
-  t.assert(expected === actual)
-  t.true(result[0].result)
+    t.ok(first.result)
+    t.equal(expected, first.message)
+    t.end()
+  })
 })
 
 test('fails an empty folder', function (t) {
-  t.plan(2)
-
   var folder = helper.createEmptyFolder('blank-folder')
-  var result = verify(folder)
+  verify(folder, function (result) {
+    var expected = 'This folder is not being tracked by Git.'
+    var first = result[0]
 
-  var expected = 'This folder is not being tracked by Git.'
-  var actual = result[0].message
-
-  t.assert(expected === actual)
-  t.false(result[0].result)
+    t.notOk(first.result)
+    t.equal(expected, first.message)
+    t.end()
+  })
 })
