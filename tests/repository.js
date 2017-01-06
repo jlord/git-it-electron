00a4b9ac1fc66302cd665e63ea67b194617a6d7e
Brendan Forster <brendan@github.com>
2017-01-06 10:36:55 +1030

    tweaking test to make it a bit more ideal

--- tests/repository.js
+++ tests/repository.js
@@ -4,19 +4,27 @@ var helper = require('./repository-helper')
 var verify = require('../lib/verify/repository')
 
 test('verifies the hello world repository', function (t) {
-  t.plan(1)
+  t.plan(2)
 
   var folder = helper.extractFixture('hello-world')
   var result = verify(folder)
 
-  t.assert(result !== null)
+  var expected = 'This is a Git repository!'
+  var actual = result[0].message
+
+  t.assert(expected === actual)
+  t.true(result[0].result)
 })
 
 test('fails an empty folder', function (t) {
-  t.plan(1)
+  t.plan(2)
 
   var folder = helper.createEmptyFolder('blank-folder')
   var result = verify(folder)
 
-  t.assert(result !== null)
+  var expected = 'This folder is not being tracked by Git.'
+  var actual = result[0].message
+
+  t.assert(expected === actual)
+  t.false(result[0].result)
 })
